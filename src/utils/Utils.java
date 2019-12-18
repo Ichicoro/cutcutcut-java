5486e7b1b088f8dcaab62b7714ff052bc8b5788b
ichicoro <me@marte.dev>
2019-12-18 14:40:33 +0100

    Clean up Utils.java

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -4,38 +4,29 @@ import actors.*;
 
 public class Utils {
 	public static boolean isNumeric(final String str) {
-
         // null or empty
-        if (str == null || str.length() == 0) {
+        if (str == null || str.length() == 0)
             return false;
-        }
-
-        for (char c : str.toCharArray()) {
-            if (!Character.isDigit(c)) {
+        
+        for (char c : str.toCharArray())
+            if (!Character.isDigit(c))
                 return false;
-            }
-        }
-
         return true;
-
     }
 	
 	public static String getActionTypeText(Action a) {
-		if (a instanceof FileSplitter) {
-			if (a instanceof FileSplitterByPartSize) {
+		if (a instanceof FileSplitter)
+			if (a instanceof FileSplitterByPartSize)
 				return "By size";
-			} else if (a instanceof FileSplitterWithEncryption) {
+			else if (a instanceof FileSplitterWithEncryption)
 				return "Encrypted";
-			} else if (a instanceof FileSplitterByPartCount) {
+			else if (a instanceof FileSplitterByPartCount)
 				return "By count";
-			}
-		} else {
-			if (a instanceof DefaultFileMerger) {
+		else
+			if (a instanceof DefaultFileMerger)
 				return "By size/count";
-			} else if (a instanceof EncryptedFileMerger) {
+			else if (a instanceof EncryptedFileMerger)
 				return "Encrypted";
-			}
-		}
 		return "";
 	}
 }
